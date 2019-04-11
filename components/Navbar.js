41c3c66b671988710aabb69fd7cee169232a0ad8
hernanalbano <hernanalbano@gmail.com>
2019-04-11 20:09:28 -0300

    Conflicts solved

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,46 +5,17 @@ import FullScreenModal from './FullScreenModal'
 import SuscribeForm from './SuscribeForm'
 
 const NavbarWrapper = styled.header`
-<<<<<<< HEAD
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 99999;
-  width: 100%;
-  background-color: #131929;
-  padding: 9px 24px;
-  height: 64px;
-  border-bottom: 1px solid rgba(255, 255, 255, 0.2);
-  button {
-    font-family: Raleway;
-  }
-  > div {
-    display: flex;
-    align-items: center;
-  }
-  > div:nth-child(1) {
-    display: none;
-  }
-  @media (min-width: 500px) {
-    > div:nth-child(1) {
-      display: block;
-    }
-    > div:nth-child(2) {
-      display: none;
-    }
-  }
-=======
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
 	position: fixed;
+	top: 0;
+  left: 0;
 	z-index: 99999;
 	width: 100%;
 	background-color: #131929;
-	padding: 14px 24px;
+	padding: 9px 24px;
+  height: 64px;
 	border-bottom: 1px solid rgba(255, 255, 255, 0.2);
 	button {
 		font-family: Raleway;
@@ -64,7 +35,6 @@ const NavbarWrapper = styled.header`
 			display: none;
 		}
 	}
->>>>>>> Corrected buttons proportions
 `
 
 function Navbar() {
