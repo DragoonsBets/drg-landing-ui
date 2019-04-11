c1b4ecea3fe3c32de734a0842991636ff298147f
rodrigo <rodrigo1987macedo@gmail.com>
2019-04-11 13:28:39 -0300

    favicon

--- components/Layout.js
+++ components/Layout.js
@@ -46,6 +46,32 @@ class Layout extends React.Component {
             rel="stylesheet"
             href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.11/semantic.min.css"
           />
+          {/* favicon */}
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="../static/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="../static/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="../static/favicon-16x16.png"
+          />
+          <link rel="manifest" href="../static/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="../static/safari-pinned-tab.svg"
+            color="#ffffff"
+          />
+          <meta name="msapplication-TileColor" content="#2d89ef" />
+          <meta name="theme-color" content="#ffffff" />
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
