d134beb72345af54dddd20f070d8799b299f144a
Pablo Garcia <pgarcia@dragoons.gg>
2019-07-13 18:51:29 -0300

    added endpint

--- networking/endpoints.js
+++ networking/endpoints.js
@@ -2,8 +2,8 @@
 const API_HOST_STAGING = 'https://drg-bets.jx-staging.viserion.dragoons.gg'
 const API_HOST = 'https://backend.viserion.dragoons.gg'
 
-export const GET_FAQ = API_HOST + '/es/faq/v1/pages/?type=bets.faqPage&fields=body';
-export const GET_FAQ_SLUG = API_HOST + '/es/faq/v1/pages/?slug=faq';
+export const GET_FAQ = API_HOST + '/es/faq/v1/pages/?type=bets.drgPage&fields=body';
+export const GET_FAQ_SLUG = API_HOST + '/es/faq/v1/pages/?slug=drg';
 export const CREATE_USERS = API_HOST + '/rest-auth/registration/';
 export const CONTACT_FORM = API_HOST + '/users/contact/'
 export const STEAM_SOCIAL_REGISTER = API_HOST + '/accounts/steam/login/?process=login';
