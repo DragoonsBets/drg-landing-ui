56f0e72413760a7a69d095c67039266a1c2fed9a
emilio <orsiemilio@gmail.com>
2019-07-13 21:06:12 -0300

    Fix landing faqs

--- networking/endpoints.js
+++ networking/endpoints.js
@@ -3,7 +3,7 @@ const API_HOST_STAGING = 'https://drg-bets.jx-staging.viserion.dragoons.gg'
 const API_HOST = 'https://backend.viserion.dragoons.gg'
 
 export const GET_FAQ = API_HOST + '/es/faq/v1/pages/?type=bets.drgPage&fields=body';
-export const GET_FAQ_SLUG = API_HOST + '/es/faq/v1/pages/?slug=drg';
+export const GET_FAQ_SLUG = API_HOST + '/es/faq/v1/pages/?slug=faq';
 export const CREATE_USERS = API_HOST + '/rest-auth/registration/';
 export const CONTACT_FORM = API_HOST + '/users/contact/'
 export const STEAM_SOCIAL_REGISTER = API_HOST + '/accounts/steam/login/?process=login';
