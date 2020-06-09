524e408c2256fced2533544b39adff997c219012
Hernan <hernanalbano@gmail.com>
2020-06-09 17:52:00 -0300

    Next export added

--- out/_next/static/chunks/styles.js
+++ out/_next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1562871100078");
+            injectCss(link, link.href.split("?")[0] + "?unix=1591727230747");
           });
       }
     }
